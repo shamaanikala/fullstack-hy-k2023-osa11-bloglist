6aa6ed87d7af5e7f98f51c16df15fa2e7d83162f
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-26 16:03:40 +0300

    Script running dev frontend. Reconfigure PORT
    React wants to use the .env variable PORT

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 
 //const PORT = 3003
-const PORT = process.env.PORT
+// Ex11.20 React uses the .env variable PORT
+const PORT = process.env.BACKEND_PORT
 
-
-const mongoUrl = process.env.NODE_ENV === 'test'
+const mongoUrl =
+  process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI
 
 module.exports = {
-    mongoUrl,
-    PORT
+  mongoUrl,
+  PORT,
 }
