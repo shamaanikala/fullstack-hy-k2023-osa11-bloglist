35c53a2d44d1fccecb267ba16d565cf080137a5f
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-27 11:31:05 +0300

    Configure production build and serve it
    Following the Create React App express
    introduction to serve the static site

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
-//const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
+const path = require('path')
 
-
-mongoose.set('strictQuery',false)
-// TODO Virheenkäsittely tälle
+mongoose.set('strictQuery', false)
 mongoose.connect(config.mongoUrl)
 
 app.use(cors())
@@ -23,15 +20,24 @@ app.use(middleware.requestLogger)
 
 app.use(middleware.tokenExtractor)
 
-app.use('/api/blogs',blogsRouter)
+app.use('/api/blogs', blogsRouter)
 // ei toimi näin, koska myös get haluaa tokenin
-//app.use('/api/blogs', middleware.userExtractor,blogsRouter) 
+//app.use('/api/blogs', middleware.userExtractor,blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
+}
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('Production env: serving build as static site')
+  app.use(express.static(path.join(__dirname, 'build')))
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  })
 }
 
 app.use(middleware.errorHandler)
