2b7bcbd65a9a473edddeacefe21c0d8f974ecbf9
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-27 14:30:56 +0300

    Fix eslint errors

--- tests/login_api.test.js
+++ tests/login_api.test.js
@@ -1,9 +1,7 @@
-const mongoose = require('mongoose')
 const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
-const Blog = require('../models/blog')
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
@@ -12,15 +10,18 @@ describe('jos POST pyynnössä on viallinen token', () => {
   beforeEach(async () => {
     await User.deleteMany({})
     const username = 'testi-root'
-    const passwordHash = await bcrypt.hash('salaisuus',10)
+    const passwordHash = await bcrypt.hash('salaisuus', 10)
 
     const user = new User({ username: username, passwordHash: passwordHash })
 
     await user.save()
 
     // toinen user
-    const passwordHash2 = await bcrypt.hash('salasana2',10)
-    const user2 = new User({ username: 'toinen-käyttäjä', passwordHash: passwordHash2 })
+    const passwordHash2 = await bcrypt.hash('salasana2', 10)
+    const user2 = new User({
+      username: 'toinen-käyttäjä',
+      passwordHash: passwordHash2,
+    })
     await user2.save()
   })
 
@@ -28,7 +29,10 @@ describe('jos POST pyynnössä on viallinen token', () => {
     test('kun huono token ton tehty jsonwebtoken.sign() funktiolla', async () => {
       // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1sdXVra2FpIiwiaWQiOiI2NDM2NWQ0YzEzNzc3NGU1NGIxZjk2NzUiLCJpYXQiOjE2ODE2NTM5NzQsImV4cCI6MTY4MTY1NzU3NH0.ioBRQtVi78j6b85kgSxNpK2aI4QBlwSHWEjDE-1URAo
       const dummyUser = { username: 'testi-root' }
-      const mockToken = jwt.sign({ username: 'testi-root', userId: 'abcd' }, process.env.SECRET)
+      const mockToken = jwt.sign(
+        { username: 'testi-root', userId: 'abcd' },
+        process.env.SECRET
+      )
 
       const result = await api
         .post('/api/login/verify-token')
@@ -42,7 +46,8 @@ describe('jos POST pyynnössä on viallinen token', () => {
 
     test('kun huono token on kopioitu jsonwebtokenin luomasta tokenista', async () => {
       const dummyUser = { username: 'testi-root' }
-      const mockToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1sdXVra2FpIiwiaWQiOiI2NDM2NWQ0YzEzNzc3NGU1NGIxZjk2NzUiLCJpYXQiOjE2ODE2NTM5NzQsImV4cCI6MTY4MTY1NzU3NH0.ioBRQtVi78j6b85kgSxNpK2aI4QBlwSHWEjDE-1URAo'
+      const mockToken =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1sdXVra2FpIiwiaWQiOiI2NDM2NWQ0YzEzNzc3NGU1NGIxZjk2NzUiLCJpYXQiOjE2ODE2NTM5NzQsImV4cCI6MTY4MTY1NzU3NH0.ioBRQtVi78j6b85kgSxNpK2aI4QBlwSHWEjDE-1URAo'
 
       const result = await api
         .post('/api/login/verify-token')
@@ -61,9 +66,8 @@ describe('jos POST-pyynnössä on kelvollinen token', () => {
     const user = { username: 'testi-root', password: 'salaisuus' }
     const loginResponse = await helper.login(user.username, user.password)
     const token = loginResponse.body.token
-    //console.log(loginResponse.body)
 
-    const result = await api
+    await api
       .post('/api/login/verify-token')
       .auth(token, { type: 'bearer' }) // tässä pitää olla bearer pienellä
       .send({ username: user.username })
@@ -78,13 +82,12 @@ describe('jos POST-pyynnössä on kelvollinen token', () => {
 
     const mockUser = { username: 'toinen-käyttäjä' }
 
-    const result = await api
+    await api
       .post('/api/login/verify-token')
       .auth(token, { type: 'bearer' }) // tässä pitää olla bearer pienellä
       .send(mockUser)
       .expect(401)
       .expect('Content-Type', /application\/json/)
-
   })
 
   test('voiko käyttää vanhaa omaa tokenia', async () => {
@@ -92,10 +95,12 @@ describe('jos POST-pyynnössä on kelvollinen token', () => {
     const loginResponse = await helper.login(user.username, user.password)
     const oldToken = loginResponse.body.token
 
+    // eslint-disable-next-line no-unused-vars
     const newLoginResponse = await helper.login(user.username, user.password)
+    // eslint-disable-next-line no-unused-vars
     const token = loginResponse.body.token
 
-    const result = await api
+    await api
       .post('/api/login/verify-token')
       .auth(oldToken, { type: 'bearer' }) // tässä pitää olla bearer pienellä
       .send({ username: user.username })
