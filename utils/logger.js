2d1ff43f3d8c3739722c1fc36b7ccdd83070411f
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-28 16:29:31 +0300

    Enable production loggin and mask password
    The password is no masked in the production
    server logs.

--- utils/logger.js
+++ utils/logger.js
@@ -1,8 +1,5 @@
 const info = (...params) => {
-  if (
-    process.env.NODE_ENV !== 'test' &&
-    process.env.NODE_ENV !== 'production'
-  ) {
+  if (process.env.NODE_ENV !== 'test') {
     console.log(...params)
   }
 }
--- utils/middleware.js
+++ utils/middleware.js
@@ -6,7 +6,12 @@ const User = require('../models/user')
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
-  logger.info('Body   ', request.body)
+  if (process.env.NODE_ENV === 'production' && request.body.password) {
+    const loggedBody = { ...request.body, password: '********' }
+    logger.info('Body   ', loggedBody)
+  } else {
+    logger.info('Body   ', request.body)
+  }
   logger.info(Date())
   logger.info('---')
   next()
@@ -16,31 +21,42 @@ const requestLogger = (request, response, next) => {
 //     response.status(404).send({ error: 'unknown endpoint '})
 // }
 
-
-const errorHandler = (error,request,response,next) => {
+const errorHandler = (error, request, response, next) => {
   //logger.error(error)
   logger.error(error.message)
 
   //logger.error(error)
-  if (error.name === 'CastError') { // PUT
+  if (error.name === 'CastError') {
+    // PUT
     //return response.status(400).send({ error: `Error while updating likes field: likes must be a number`})
-    return response.status(400).send({ error: `Error: ${error.name}: ${error.message}` })
+    return response
+      .status(400)
+      .send({ error: `Error: ${error.name}: ${error.message}` })
   } else if (error.name === 'ValidationError') {
     // https://mongoosejs.com/docs/validation.html#validation-errors
     //console.log(error.errors)
     if (error.errors.likes) {
       //logger.error(error.errors.likes.name) // CastError
       logger.error(error.errors.likes.message)
-      return response.status(400).send({ error: 'likes field must be a number' })
+      return response
+        .status(400)
+        .send({ error: 'likes field must be a number' })
     } else if (error.errors.url) {
       logger.error(error.errors.url.message)
-      return response.status(400).send({ error: 'url field can\'t be empty' })
+      // eslint-disable-next-line quotes
+      return response.status(400).send({ error: "url field can't be empty" })
     } else if (error.errors.title) {
       logger.error(error.errors.title.message)
-      return response.status(400).send({ error: 'title field can\'t be empty' })
-    } else if (error.errors.username.message === 'Username must have at least 3 characters') {
+      // eslint-disable-next-line quotes
+      return response.status(400).send({ error: "title field can't be empty" })
+    } else if (
+      error.errors.username.message ===
+      'Username must have at least 3 characters'
+    ) {
       logger.error(error.errors.username.message)
-      return response.status(400).send({ error: `Username must have at least 3 characters (given username was: '${error.errors.username.value}')` })
+      return response.status(400).send({
+        error: `Username must have at least 3 characters (given username was: '${error.errors.username.value}')`,
+      })
     }
     //console.log(`Required fields missing ${Object.keys(error.errors)}`)
     //return response.status(400).send({error: `Required fields missing ${Object.keys(error.errors)}`})
@@ -96,5 +112,5 @@ module.exports = {
   //unknownEndpoint,
   errorHandler,
   tokenExtractor,
-  userExtractor
+  userExtractor,
 }
